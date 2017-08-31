f5ee92355ee1caf4504b99f37580cd59c8898591
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-09-01 08:18:20 +0900

    improve 3

--- MacroParser.cpp
+++ MacroParser.cpp
@@ -2,25 +2,30 @@
 
 int main(void)
 {
-    using namespace MacroParser;
-    StringScanner scanner("123+444*2++2");
-    TokenStream stream(scanner);
-    if (!stream.read_tokens())
     {
-        std::printf("scanner failed\n");
-    }
-    else
-    {
-        stream.print();
-        Parser parser(stream);
-        if (!parser.parse())
+        using namespace MacroParser;
+        StringScanner scanner("123+444*2++2");
+        TokenStream stream(scanner);
+        if (!stream.read_tokens())
         {
-            std::printf("parser failed\n");
+            std::printf("scanner failed\n");
         }
         else
         {
-            parser.ast()->print();
+            stream.print();
+            Parser parser(stream);
+            if (!parser.parse())
+            {
+                std::printf("parser failed\n");
+            }
+            else
+            {
+                parser.ast()->print();
+            }
         }
     }
+
+    assert(MacroParser::BaseAst::alive_count() == 0);
+
     return 0;
 }
